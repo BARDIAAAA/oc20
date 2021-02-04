c6895878132e37d4a0991b1ff5ae3b0656ace25f
BARDIAAAA <bardia.orang@eduvaud.ch>
2021-02-04 14:00:56 +0100

    Update working files

--- tutorial/lesson 3/rect.py
+++ tutorial/lesson 3/rect.py
@@ -1,26 +1,25 @@
-from rect import *
+import pygame
+from pygame.locals import *
+from random import randint
 
-def draw_point(text, pos):
-    img = font.render(text, True, BLACK)
-    pygame.draw.circle(screen, RED, pos, 3)
-    screen.blit(img, pos)
+width = 500
+height = 200
 
-rect = Rect(50, 40, 250, 80)
-pts = ('topleft', 'topright', 'bottomleft', 'bottomright',
-        'midtop', 'midright', 'midbottom', 'midleft', 'center')
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+
+BLACK = (0, 0, 0)
+GRAY = (150, 150, 150)
+WHITE = (255, 255, 255)
+
+dir = {K_LEFT: (-5, 0), K_RIGHT: (5, 0), K_UP: (0, -5), K_DOWN: (0, 5)}
+
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+font = pygame.font.Font(None, 24)
 running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-
-    screen.fill(GRAY)
-    pygame.draw.rect(screen, GREEN, rect, 4)
-
-    for pt in pts:
-        draw_point(pt, eval('rect.'+pt))
-
-    pygame.display.flip()
-
-pygame.quit()
