eb364c4f03664c6a9099d99b4a9923dba443d5ea
Bardia <bardia.orang@eduvaud.ch>
2021-06-01 22:49:31 +0200

    Adding moving pac-man with different keys / speed / time.sleep

--- editor/main.py
+++ editor/main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 
+
 class color:
     def __init__(self):
         self.black = (0, 0, 0)
@@ -10,85 +11,108 @@ class color:
         self.white = (255, 255, 255)
         self.blue = (0, 0, 255)
 
+
 class segment:
     def __init__(self):
         self.start = (0, 0)
         self.end = (0, 0)
 
-    def setStart(position):
-        segment.start = position
-
-    def setEnd(position):
-        segment.end = position
-
     def getSegment():
-        list = []
-        list.append(segment.start)
-        list.append(segment.end)
-        return list
+        line = []
+        line.append(segment.start)
+        line.append(segment.end)
+        return line
+
+
+class image:
+    def __init__(self):
+        self.img = pygame.image.load('bird.png')
+        self.moving = False
+        self.rect = self.img.get_rect()
+        self.x = 250
+        self.y = 250
+
 
 class main:
-    "Lancement du logiciel"
     def __init__(self):
         self.color = color()
         self.segment = segment()
+        self.image = image()
         self.undo = []
         self.list_lines = []
         self.background = self.color.white
+        self.key_dict = {
+            K_r: self.color.red,
+            K_g: self.color.gray,
+            K_v: self.color.green,
+            K_w: self.color.white,
+            K_b: self.color.blue
+        }
+        self.screen = pygame.display.set_mode((800, 800))
 
-        # Brouillon
-        key_dict = {K_r:self.color.red, K_g:self.color.gray, K_v:self.color.green, K_w:self.color.white, K_b:self.color.blue}
         pygame.init()
-        screen = pygame.display.set_mode((800, 800))
-        img = pygame.image.load('bird.png')
-        img.convert()
-        rect = img.get_rect()
-        rect.center = w, h = 400, 400
-        moving = False
+        self.image.img.convert()
 
         while True:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
-
                     if event.key == K_n:
-                        if (moving):
-                            moving = False
+                        if (self.image.moving):
+                            self.image.moving = False
                         else:
-                            moving = True
+                            self.image.moving = True
+
+                    elif event.key == K_UP:
+                        self.image.y -= 10
+                    elif event.key == K_DOWN:
+                        self.image.y += 10
+                    elif event.key == K_LEFT:
+                        self.image.x -= 10
+                    elif event.key == K_RIGHT:
+                        self.image.x += 10
+
+                    elif event.key == K_k:
+                        self.image.x = 250
+                        self.image.y = 250
 
                     elif event.key == K_z:
                         if (len(self.list_lines) > 0):
                             self.undo.append(self.list_lines[-1])
                             self.list_lines.pop()
-
                     elif event.key == K_y:
-                        if (len(self.undo) > 1):
+                        if (len(self.undo) > 0):
                             self.list_lines.append(self.undo[-1])
                             self.undo.pop()
+
                     else:
-                        for key in key_dict.keys():
+                        for key in self.key_dict.keys():
                             if event.key == key:
-                                self.background = key_dict.get(key)
+                                self.background = self.key_dict.get(key)
 
-                elif event.type == MOUSEMOTION and moving:
-                    rect.move_ip(event.rel)
+                elif event.type == MOUSEMOTION and self.image.moving:
+                    convert = self.image.img.convert()
+                    h, w = convert.get_height(), convert.get_width()
+                    x, y = pygame.mouse.get_pos()
 
-                elif event.type == MOUSEBUTTONDOWN:
-                    segment.setStart(event.pos)                  
-                elif event.type == MOUSEBUTTONUP:
-                    segment.setEnd(event.pos)
-                    self.list_lines.append(segment.getSegment())
+                    self.image.x, self.image.y = x - h / 2, y - w / 2
 
-            screen.fill(self.background)
-            screen.blit(img, rect)
+                elif not self.image.moving:
+                    if event.type == MOUSEBUTTONDOWN:
+                        segment.start = event.pos
+                    elif event.type == MOUSEBUTTONUP:
+                        segment.end = event.pos
+                        self.list_lines.append(segment.getSegment())
+
+            self.screen.fill(self.background)
+            self.screen.blit(self.image.img, (self.image.x, self.image.y))
 
             if len(self.list_lines) > 0:
                 for line_i in self.list_lines:
-                    pygame.draw.lines(screen, self.color.black, True, line_i, 3)
+                    pygame.draw.lines(self.screen, self.color.black, True, line_i, 3)
 
             pygame.display.update()
 
     pygame.quit()
 
-print(main.__doc__)
-main() 
+
+main()
--- game/main.py
+++ game/main.py
@@ -1,7 +1,9 @@
+import time
+
 import pygame
 
 
-class Color: # Création de la classe des couleurs.
+class Color:  # Création de la classe des couleurs.
     def __init__(self):
         self.BLACK = (0, 0, 0)
         self.RED = (255, 0, 0)
@@ -14,7 +16,7 @@ class Color: # Création de la classe des couleurs.
         self.PURPLE = (102, 102, 255)
 
 
-class Path: # Chargement de toutes les entités / musique / police
+class Path:  # Chargement de toutes les entités / musique / police
     def __init__(self):
         self.ICON = pygame.image.load('resources/images/pacman.png')
         self.PACMAN = pygame.image.load('resources/images/pacman.png')
@@ -26,90 +28,75 @@ class Path: # Chargement de toutes les entités / musique / police
         self.MUSIC = 'resources/music/pac-man-theme.wav'
 
 
-class Wall: # Création de la classe "Wall" (Mur) qui va permettre d'implanter les murs que l'on souhaite
+class Wall:  # Création de la classe "Wall" (Mur) qui va permettre d'implanter les murs que l'on souhaite
     def __init__(self, screen, color, coords):
         pygame.draw.rect(screen, color, coords)
 
 
-class Fantom: # Création de la classe "Fantom" qui va permettre d'implanter les fantômes du jeu
-    def __init__(self, screen, image, x, y):
+class Fantom:  # Création de la classe "Fantom" qui va permettre d'implanter les fantômes du jeu
+    def __init__(self, image, x, y):
+        self.image = image
         self.x = x
         self.y = y
-        screen.blit(image, (x, y))
 
 
-class Hero: # Création de la classe "Hero" qui va permettre d'implanter Pac-Man
-    def __init__(self, screen, image, x, y):
-        self.pacman = pygame.image.load('resources/images/pacman.png')
+class Hero:  # Création de la classe "Hero" qui va permettre d'implanter Pac-Man
+    def __init__(self, x, y):
+        self.image = pygame.image.load('resources/images/pacman.png')
         self.x = x
         self.y = y
         self.moving = False
-        screen.blit(image, (x, y))
+        self.moving_direction = "x"
+        self.moving_speed = 25
 
- #   def setMoving(self, boolean):
-  #      self.moving = boolean
 
-   # def getX(self):
-    #    return self.x
-
-    #def getY(self):
-     #   return self.y
-
-    #def canMove(self):
-     #   return self.moving
-
-    #def setX(self, int):
-     #   self.x = int
-
-    #def setY(self, int):
-     #   self.y = int
-
-class Game: # Classe la plus importante, celle qui implante les murs / choisis les spawns des fantômes et de Pac-Man
+class Game:  # Classe la plus importante, celle qui implante les murs / choisis les spawns des fantômes et de Pac-Man
     def __init__(self):
         self.color = Color()
         self.path = Path()
         self.screen = pygame.display.set_mode((535, 600))
         self.walls = [
-            (0, 0, 600, 10), # Ligne tout en haut
-            (0, 0, 10, 600), # Ligne tout à gauche
-            (525, 0, 10, 600), # Ligne tout à gauche
-            (0, 590, 600, 10), # Ligne tout en bas
-            (265, 0, 10, 160), # Ligne verticale centrale haut
-            (265, 450, 10, 150), # Ligne verticale centrale bas
-            (0, 295, 150, 10), # Ligne horizontale centrale gauche
-            (390, 295, 150, 10), # Ligne horizontale centrale droite
-            (330, 270, 10, 60), # Cage, mur droite
-            (200, 270, 10, 60), # Cage, mur gauche
-            (200, 330, 140, 10), # Cage, mur bas
-            (200, 270, 45, 10), # Cage, mur haut gauche
-            (295, 270, 45, 10), # Cage, mur haut droite
-            (200, 390, 140, 10), # Ligne horizontale centrale bas
-            (200, 210, 140, 10), # Ligne horizontale centrale haut
-            (200, 65, 10, 150), # Ligne verticale centrale gauche haut
-            (330, 65, 10, 150), # Ligne verticale centrale droite haut
-            (200, 390, 10, 145), # Ligne verticale centrale gauche bas
-            (330, 390, 10, 145), # Ligne verticale centrale droite bas
-            (65, 65, 10, 175), # Ligne verticale gauche gauche haut
-            (460, 65, 10, 175), # Ligne verticale droite droite haut
-            (65, 360, 10, 175), # Ligne verticale gauche gauche bas
-            (460, 360, 10, 175), # Ligne verticale droite droite bas
-            (65, 65, 80, 10), # Ligne horizontale gauche haut
-            (130, 130, 10, 110), # Ligne verticale gauche haut seule
-            (65, 525, 80, 10), # Ligne horizontale gauche bas
-            (130, 360, 10, 110), # Ligne verticale gauche bas seule
-            (392, 65, 75, 10), # Ligne horizontale droite haut
-            (395, 130, 10, 110), # Ligne verticale droite haut seule
-            (392, 525, 75, 10), # Ligne horizontale droite bas
-            (395, 360, 10, 110), # Ligne verticale droite bas seule
+            (0, 0, 600, 10),  # Ligne tout en haut
+            (0, 0, 10, 600),  # Ligne tout à gauche
+            (525, 0, 10, 600),  # Ligne tout à gauche
+            (0, 590, 600, 10),  # Ligne tout en bas
+            (265, 0, 10, 160),  # Ligne verticale centrale haut
+            (265, 450, 10, 150),  # Ligne verticale centrale bas
+            (0, 295, 150, 10),  # Ligne horizontale centrale gauche
+            (390, 295, 150, 10),  # Ligne horizontale centrale droite
+            (330, 270, 10, 60),  # Cage, mur droite
+            (200, 270, 10, 60),  # Cage, mur gauche
+            (200, 330, 140, 10),  # Cage, mur bas
+            (200, 270, 45, 10),  # Cage, mur haut gauche
+            (295, 270, 45, 10),  # Cage, mur haut droite
+            (200, 390, 140, 10),  # Ligne horizontale centrale bas
+            (200, 210, 140, 10),  # Ligne horizontale centrale haut
+            (200, 65, 10, 150),  # Ligne verticale centrale gauche haut
+            (330, 65, 10, 150),  # Ligne verticale centrale droite haut
+            (200, 390, 10, 145),  # Ligne verticale centrale gauche bas
+            (330, 390, 10, 145),  # Ligne verticale centrale droite bas
+            (65, 65, 10, 175),  # Ligne verticale gauche gauche haut
+            (460, 65, 10, 175),  # Ligne verticale droite droite haut
+            (65, 360, 10, 175),  # Ligne verticale gauche gauche bas
+            (460, 360, 10, 175),  # Ligne verticale droite droite bas
+            (65, 65, 80, 10),  # Ligne horizontale gauche haut
+            (130, 130, 10, 110),  # Ligne verticale gauche haut seule
+            (65, 525, 80, 10),  # Ligne horizontale gauche bas
+            (130, 360, 10, 110),  # Ligne verticale gauche bas seule
+            (392, 65, 75, 10),  # Ligne horizontale droite haut
+            (395, 130, 10, 110),  # Ligne verticale droite haut seule
+            (392, 525, 75, 10),  # Ligne horizontale droite bas
+            (395, 360, 10, 110),  # Ligne verticale droite bas seule
         ]
-        self.fantoms = [#Coordonnées des points de spawns des fantômes
-            [self.path.BLINKY, 280, 289],
-            [self.path.CLYDE, 280, 289],
-            [self.path.INKY, 230, 289],
-            [self.path.PINKY, 230, 289]
+        self.fantoms = [  # Coordonnées des points de spawns des fantômes
+            Fantom(self.path.BLINKY, 280, 289),
+            Fantom(self.path.CLYDE, 280, 289),
+            Fantom(self.path.INKY, 230, 289),
+            Fantom(self.path.PINKY, 230, 289)
         ]
-        self.heros = [#Coordonnées des points de spawn de Pac-Man
-            [self.path.PACMAN, 220, 550]
+        self.heros = [  # Coordonnées des points de spawn de Pac-Man
+            #[self.path.PACMAN, 220, 550]
+            Hero(220, 550)
         ]
 
     def init(self):
@@ -119,35 +106,58 @@ class Game: # Classe la plus importante, celle qui implante les murs / choisis l
         pygame.display.set_mode((535, 600))
         self.screen.fill(self.color.BLACK)
         while True:
-            for wall in self.walls:
-                Wall(self.screen, self.color.CIEL, wall)
+            self.screen.fill(self.color.BLACK)
+
             # Spawn les fantômes
-            for fantom in self.fantoms:
-                Fantom(self.screen, fantom[0], fantom[1], fantom[2])
             # Spawn Pac-Man
             for hero in self.heros:
-                player = Hero(self.screen, hero[0], hero[1], hero[2])
-         #   for event in pygame.event.get():
-          #          if event.type == pygame.KEYDOWN:
-           #             if event.key == pygame.K_LEFT:
-            #                player.setMoving(True)
-             #               player.setX(player.getX() - 1)
-              #      if event.type == pygame.KEYUP:
-               #         if player.canMove():
-                #            player.setMoving(False)
-                #            player.setMoving(False)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                            hero.moving = True
+                            hero.moving_direction = "y"
+                            hero.moving_speed = -25
+                        if event.key == pygame.K_DOWN:
+                            hero.moving = True
+                            hero.moving_direction = "y"
+                            hero.moving_speed = 25
+                        if event.key == pygame.K_LEFT:
+                            hero.moving = True
+                            hero.moving_direction = "x"
+                            hero.moving_speed = -25
+                        if event.key == pygame.K_RIGHT:
+                            hero.moving = True
+                            hero.moving_direction = "x"
+                            hero.moving_speed = 25
+                    if event.type == pygame.KEYUP:
+                        if hero.moving:
+                            hero.moving = False
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+
+
+                if hero.moving:
+                    time.sleep(0.09)
+                    if hero.moving_direction == "x":
+                        hero.x = hero.x + hero.moving_speed
+                    if hero.moving_direction == "y":
+                        hero.y = hero.y + hero.moving_speed
+
+                self.screen.blit(hero.image, (hero.x, hero.y))
+
+            for fantom in self.fantoms:
+                self.screen.blit(fantom.image, (fantom.x, fantom.y))
+            for wall in self.walls:
+                Wall(self.screen, self.color.CIEL, wall)
+
             pygame.display.update()
 
 
-
-pygame.mixer.init()
-pygame.mixer.music.load(Path().MUSIC)
-pygame.mixer.music.play(-1)
-pygame.mixer.music.set_volume(0.1)
+#pygame.mixer.init()
+#pygame.mixer.music.load(Path().MUSIC)
+#pygame.mixer.music.play(-1)
+#pygame.mixer.music.set_volume(0.1)
 
 game = Game()
 game.init()
